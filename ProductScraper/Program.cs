fffafff469c2f9cabdcbc34d84704a0132cb0478
Richard Leurs <7275740+EarthlingRich@users.noreply.github.com>
2018-10-29 22:23:56 +0100

    Add project files.

---
+++ ProductScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

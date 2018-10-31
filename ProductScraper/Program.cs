b19a4a51b790618771cea74a8df00db4ea1d98ca
Richard Leurs <leursrichard@gmail.com>
2018-10-31 19:54:48 +0100

    Traverse Albert Heijn categories to scrape products.

--- ProductScraper/Program.cs
+++ ProductScraper/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 
 namespace ProductScraper
 {
@@ -9,18 +11,54 @@ namespace ProductScraper
         static void Main(string[] args)
         {
             var products = new List<Product>();
+            var productsUrls = new List<string>();
 
-            var url = "https://www.ah.nl/producten/product/wi132400/ah-extra-jam-aardbeien"; //Test product url
+            var url = "https://www.ah.nl/producten/ontbijtgranen-broodbeleg-tussendoor/"; //Test product category with sub categories url
 
-            using(var driver = new ChromeDriver())
+            using (var driver = new ChromeDriver())
             {
-                driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 10);
-                driver.Navigate().GoToUrl(url);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                productsUrls.AddRange(GetProductsUrls(url, driver));
 
-                var product = new Product();
-                product.Name = driver.FindElementByXPath("//h1[contains(@class, 'product-description__title')]").Text;
-                products.Add(product);
+                foreach(string productUrl in productsUrls) {
+                    products.Add(GetProduct(productUrl, driver));
+                }
             }
+        }
+
+        static List<string> GetProductsUrls(string url, ChromeDriver driver) {
+            var productUrls = new List<string>();
+            driver.Navigate().GoToUrl(url);
+
+            //Scroll to footer to load all categories and products
+            var footer = driver.FindElementByXPath("//footer");
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(footer);
+            actions.Perform();
+
+            //Get product category url's, keep try loading sub categories unitl no categories are found
+            var productCategoryUrls = driver.FindElementsByXPath("//div[contains(@class, 'product-lane')]//div[contains(@class, 'legend')]//a[contains(@class, 'grid-item__content')]").Select(_ => _.GetAttribute("href")).ToList();
+            if (productCategoryUrls.Any()) {
+                foreach (string productCategoryUrl in productCategoryUrls)
+                {
+                    productUrls.AddRange(GetProductsUrls(productCategoryUrl, driver));
+                }
+            }
+
+            //If there are no sub categories found load get product url's
+            productUrls.AddRange(driver.FindElementsByXPath("//a[contains(@class, 'product__content--link')]").Select(_ => _.GetAttribute("href")).ToList());
+            return productUrls;
+        }
+
+        static Product GetProduct(string url, ChromeDriver driver) {
+            driver.Navigate().GoToUrl(url);
+
+            var product = new Product
+            {
+                Name = driver.FindElementByXPath("//h1[contains(@class, 'product-description__title')]").Text
+            };
+
+            return product;
         }
     }
 }

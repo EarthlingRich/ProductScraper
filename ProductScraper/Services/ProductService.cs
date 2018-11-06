fe8bfe81bbe8481e175a66e52f77390252b001a8
Richard Leurs <leursrichard@gmail.com>
2018-11-06 21:44:37 +0100

    Create unit of work in constructor.

--- ProductScraper/Services/ProductService.cs
+++ ProductScraper/Services/ProductService.cs
@@ -1,33 +1,34 @@
 ﻿using ProductScraper.Models;
 using ProductScraper.Persistance;
+using ProductScraper.Persistance.Interfaces.Repositories;
 
 namespace ProductScraper.Services
 {
     public class ProductService
     {
+        readonly IUnitOfWork _unitOfWork;
+
+        public ProductService() {
+            _unitOfWork = new UnitOfWork(new ApplicationContext());
+        }
+
         public void Add(Product product)
         {
-            using (var unitOfWork = new UnitOfWork(new ApplicationContext()))
-            {
-                unitOfWork.Products.Add(product);
-            }
+            _unitOfWork.Products.Add(product);
+            _unitOfWork.Complete();
         }
 
         public void UpdateOrAdd(Product product)
         {
-            using (var unitOfWork = new UnitOfWork(new ApplicationContext()))
+            var existingProduct = _unitOfWork.Products.FirstOrDefault(_ => _.Url == product.Url);
+            if (existingProduct == null)
             {
-                var existingProduct = unitOfWork.Products.FirstOrDefault(_ => _.Url == product.Url);
-                if (existingProduct == null)
-                {
-                    Add(product);
-                }
-                else
-                {
-                    existingProduct.Name = product.Name;
-                }
-
-                unitOfWork.Complete();
+                Add(product);
+            }
+            else
+            {
+                existingProduct.Name = product.Name;
+                _unitOfWork.Complete();
             }
         }
     }

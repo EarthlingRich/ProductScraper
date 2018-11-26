cee0107d2108279f885f25462cf23ce35c44c59a
Richard Leurs <leursrichard@gmail.com>
2018-11-26 13:02:01 +0100

    Move product list to new page.

--- Api/Controllers/HomeController.cs
+++ Api/Controllers/HomeController.cs
@@ -1,22 +1,12 @@
-﻿using Api.Persistance.Interfaces.Repositories;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
-        readonly IUnitOfWork _unitOfWork;
-
-        public HomeController(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
-
         public IActionResult Index()
         {
-            var products = _unitOfWork.Products.GetAll();
-
-            return View(products);
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Api/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Api.Persistance.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class ProductController : Controller
+    {
+        readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _unitOfWork.Products.GetAll();
+
+            return View(products);
+        }
+    }
+}

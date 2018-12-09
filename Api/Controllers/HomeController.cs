74aa1bc812118ebeb0b522b02c1f552d02438d33
Richard Leurs <leursrichard@gmail.com>
2018-12-09 22:46:24 +0100

    Add home cards.

--- Api/Controllers/HomeController.cs
+++ Api/Controllers/HomeController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Model;
 
 namespace Api.Controllers
 {
     public class HomeController : Controller
     {
+        readonly ApplicationContext _context;
+
+        public HomeController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var viewModel = new HomeViewModel
+            {
+                TotalProduccs = _context.Products.Count(),
+                TotalVeganProducts = _context.Products.Where(_ => _.IsVegan).Count(),
+                TotalWorkload = _context.Products.Where(_ => !_.IsProcessed).Count()
+            };
+
+            return View(viewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Api/Models/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Api.Models
+{
+    public class HomeViewModel
+    {
+        public int TotalProduccs { get; set; }
+        public int TotalVeganProducts { get; set; }
+        public int TotalWorkload { get; set; }
+    }
+}

8907612613cb39168f29ea9b14384b9827c6dd14
Richard Leurs <leursrichard@gmail.com>
2018-12-11 16:15:24 +0100

    Add products api.

---
+++ Api/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Api.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Model;
+using Model.Models;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        readonly IMapper _mapper;
+        readonly ApplicationContext _context;
+
+        public ProductsController(ApplicationContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/products
+        [HttpGet]
+        public IEnumerable<ProductApiViewModel> Get()
+        {
+            var products = _context.Products.AsNoTracking().ToList();
+            var data = products.Select(_ => _mapper.Map<ProductApiViewModel>(_));
+
+            return data;
+        }
+
+        // GET api/products/5
+        [HttpGet("{id}")]
+        public ProductApiViewModel Get(int id)
+        {
+            var product = _context.Products.AsNoTracking().FirstOrDefault(_ => _.Id == id);
+            var data = _mapper.Map<ProductApiViewModel>(product);
+
+            return data;
+        }
+    }
+}
--- Api/Models/ProductViewModel.cs
+++ Api/Models/ProductViewModel.cs
@@ -20,4 +20,15 @@ namespace Api.Models
         public bool IsProcessed { get; set; }
         public bool IsNew { get; set; }
     }
+
+    public class ProductApiViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Ingredients { get; set; }
+        public bool IsVegan { get; set; }
+        public bool IsProcessed { get; set; }
+        public bool IsNew { get; set; }
+    }
 }

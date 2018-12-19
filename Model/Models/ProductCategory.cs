9411dd01f858713e966eb080b75639ae48a21aec
Richard Leurs <leursrichard@gmail.com>
2018-12-19 22:14:27 +0100

    Add StoreCategories to ProductCategory.

--- Model/Models/ProductCategory.cs
+++ Model/Models/ProductCategory.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Model.Models
 {
     public class ProductCategory
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<StoreCategory> StoreCategories { get; set; }
     }
 }
---
+++ Model/Models/StoreCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model.Models
+{
+    [Table("StoreCategories")]
+    public class StoreCategory
+    {
+        public int Id { get; set; }
+        public ProductCategory ProductCategory { get; set; }
+        public StoreType StoreType { get; set; }
+        public string Url { get; set; }
+    }
+}

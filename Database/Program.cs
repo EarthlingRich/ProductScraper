150a871ecb268048e6f422b31a2f4b6cd1cf27ea
Richard Leurs <leursrichard@gmail.com>
2018-12-23 18:09:31 +0100

    Running Database project will migrate database.

--- Database/Program.cs
+++ Database/Program.cs
@@ -6,25 +6,35 @@ using Model;
 
 namespace Database
 {
-    public class Program  : IDesignTimeDbContextFactory<ApplicationContext>
+    public class Program : IDesignTimeDbContextFactory<ApplicationContext>
     {
         public ApplicationContext CreateDbContext(string[] args)
+        {
+            var builder = GetOptionsBuilder();
+            return new ApplicationContext(builder.Options);
+        }
+
+        static void Main(string[] args)
+        {
+            var builder = GetOptionsBuilder();
+            using (var context = new ApplicationContext(builder.Options))
+            {
+                context.Database.Migrate();
+            }
+        }
+
+        static DbContextOptionsBuilder<ApplicationContext> GetOptionsBuilder()
         {
             IConfigurationRoot configuration = new ConfigurationBuilder()
                 .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
                 .AddJsonFile("appsettings.json")
                 .AddJsonFile("appsettings.Development.json", optional: true)
                 .Build();
-                
+
             var builder = new DbContextOptionsBuilder<ApplicationContext>();
             builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), _ => _.MigrationsAssembly("Database"));
 
-            return new ApplicationContext(builder.Options);
-        }
-
-        static void Main(string[] args)
-        {
-            //Dummy console app to run dotnet ef commands
+            return builder;
         }
     }
 }

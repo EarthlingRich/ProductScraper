fc84c64da0803d24119231568b21133017cde006
Richard Leurs <leursrichard@gmail.com>
2018-12-24 20:18:23 +0100

    Fix delete ingredient keywords.

--- Api/Views/Ingredient/Ingredient.js
+++ Api/Views/Ingredient/Ingredient.js
@@ -1,56 +1,49 @@
-﻿var keywordsChips = null;
-var allergykeywordsChips = null;
-
-export function initUpdate(keywords, allergyKeywords) {
+﻿export function initUpdate(keywords, allergyKeywords) {
     Utils.initSelect();
 
     var keywordsChipsElement = document.querySelector('#keywords-chips');
 
     var options = {
         data: keywords,
+        placeholder: 'Ingrediënten',
+        secondaryPlaceholder: 'ingrediënt',
         onChipAdd: function() {
-            UpdateKeywordsString();
+            UpdateKeywordsString('Request_KeywordsString', this);
         },
         onChipDelete: function() {
-            UpdateKeywordsString();
+            UpdateKeywordsString('Request_KeywordsString', this);
         }
     }
 
-    keywordsChips = M.Chips.init(keywordsChipsElement, options);
+    M.Chips.init(keywordsChipsElement, options);
 
     var allergykeywordsChipsElement = document.querySelector('#allergykeywords-chips');
 
     var optionsAllergy = {
         data: allergyKeywords,
+        placeholder: 'Allergiën',
+        secondaryPlaceholder: 'allergie',
         onChipAdd: function() {
-            UpdateAllergyKeywordsString();
+            UpdateKeywordsString('Request_AllergyKeywordsString', this);
         },
         onChipDelete: function() {
-            UpdateAllergyKeywordsString();
+            UpdateKeywordsString('Request_AllergyKeywordsString', this);
         }
     }
 
-    allergykeywordsChips = M.Chips.init(allergykeywordsChipsElement, optionsAllergy);
+    M.Chips.init(allergykeywordsChipsElement, optionsAllergy);
 }
 
-function UpdateKeywordsString() {
+function UpdateKeywordsString(id, chips) {
     var data = '';
 
-    keywordsChips.chipsData.forEach(function(item) {
+    chips.chipsData.forEach(function(item) {
         data += item.tag + ';';
     });
-    data = data.substring(0, data.length - 1);
-    var keyWordsStringElement = document.getElementById('Request_KeywordsString');
-    keyWordsStringElement.value = data;
-}
 
-function UpdateAllergyKeywordsString() {
-    var data = '';
-
-    allergykeywordsChips.chipsData.forEach(function(item) {
-        data += item.tag + ';';
-    });
+    //Remove last semicolon
     data = data.substring(0, data.length - 1);
-    var keyWordsStringElement = document.getElementById('Request_AllergyKeywordsString');
+
+    var keyWordsStringElement = document.getElementById(id);
     keyWordsStringElement.value = data;
 }

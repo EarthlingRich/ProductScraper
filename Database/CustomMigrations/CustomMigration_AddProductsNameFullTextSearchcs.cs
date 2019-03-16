30a8fe7183e8cc60da425aaf581c5b01d0c30ea5
Richard Leurs <leursrichard@gmail.com>
2019-03-17 00:19:21 +0100

    Enable full text search for product names.

---
+++ Database/CustomMigrations/CustomMigration_AddProductsNameFullTextSearchcs.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Model;
+
+namespace Database.CustomMigrations
+{
+    [DbContext(typeof(ApplicationContext))]
+    [Migration("CustomMigration_AddProductsNameFullTextSearch")]
+    public class AddProductsNameFullTextSearch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE FULLTEXT CATALOG ProductsFullTextCatalog", true);
+            migrationBuilder.Sql("CREATE FULLTEXT INDEX ON [dbo].[Products] ([Name]) KEY INDEX PK_Products ON ProductsFullTextCatalog WITH CHANGE_TRACKING AUTO;", true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FULLTEXT INDEX ON [dbo].[Products]", true);
+            migrationBuilder.Sql("DROP FULLTEXT CATALOG ProductsFullTextCatalog", true);
+        }
+    }
+}

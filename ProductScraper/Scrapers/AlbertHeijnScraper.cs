dd4ebb9433f07ce4734e127e0395b2dc613b3085
Richard Leurs <7275740+EarthlingRich@users.noreply.github.com>
2019-12-23 19:33:58 +0100

    Fix AlbertHeijn scraper for new ah.nl design.

--- ProductScraper/Scrapers/AlbertHeijnScraper.cs
+++ ProductScraper/Scrapers/AlbertHeijnScraper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AngleSharp;
@@ -13,12 +14,13 @@ using Microsoft.EntityFrameworkCore;
 using Model;
 using Model.Models;
 using Model.Requests;
-using Newtonsoft.Json.Linq;
 
 namespace ProductScraper.Scrapers
 {
     public class AlbertHeijnScraper : IProductScraper
     {
+        readonly string BaseUrl = "https://www.ah.nl";
+
         readonly ApplicationContext _context;
         readonly ProductApplicationService _productService;
         readonly DateTime _scrapeDate;
@@ -53,11 +55,11 @@ namespace ProductScraper.Scrapers
                 {
                     if (productUrlDictonary.ContainsKey(productCategorie.Id))
                     {
-                        productUrlDictonary[productCategorie.Id].AddRange(await GetProductUrls(productCategorieUrl));
+                        productUrlDictonary[productCategorie.Id].AddRange(await GetProductUrls(productCategorieUrl, productCategorie));
                     }
                     else
                     {
-                        productUrlDictonary.Add(productCategorie.Id, await GetProductUrls(productCategorieUrl));
+                        productUrlDictonary.Add(productCategorie.Id, await GetProductUrls(productCategorieUrl, productCategorie));
                     }
                 }
             }
@@ -95,74 +97,54 @@ namespace ProductScraper.Scrapers
             await HandleProduct(product.Url, product.ProductCategories.First());
         }
 
-        async Task<List<string>> GetProductUrls(string url)
+        async Task<List<string>> GetProductUrls(string slug, ProductCategory productCategorie)
         {
+            var logLineStart = $"Scraping category: {productCategorie.Name}";
+            Console.WriteLine(logLineStart);
+            _streamWriter.WriteLine(logLineStart);
+
             var productUrls = new List<string>();
-            var productCategoryUrls = new List<string>();
-            var productLanes = new List<JToken>();
+            var taxonomyIds = new List<string>();
+            var url = $"{BaseUrl}/zoeken/api/products/search?taxonomySlug={slug}&size=10000";
 
             try
             {
-                using (var result = await _client.GetAsync("https://www.ah.nl/service/rest" + url))
+                using (var httpResponse = await _client.GetAsync(url))
+                using (var stream = await httpResponse.Content.ReadAsStreamAsync())
+                using (var data = JsonDocument.Parse(stream))
                 {
-                    using (var content = result.Content)
+                    foreach (var taxonomy in data.RootElement.GetProperty("aggregation").GetProperty("taxonomies").EnumerateArray())
                     {
-                        string dataString = await content.ReadAsStringAsync();
-                        if (dataString != null)
+                        taxonomyIds.Add(taxonomy.GetProperty("id").ToString());
+                    }
+                }
+                if (taxonomyIds.Any())
+                {
+                    foreach (string taxonomyId in taxonomyIds)
+                    {
+                        url = $"{BaseUrl}/zoeken/api/products/search?taxonomySlug={slug}&taxonomy={taxonomyId}&size=10000";
+                        using (var httpResponse = await _client.GetAsync(url))
+                        using (var stream = await httpResponse.Content.ReadAsStreamAsync())
+                        using (var data = JsonDocument.Parse(stream))
                         {
-                            var data = JObject.Parse(dataString);
-                            productLanes = data["_embedded"]?["lanes"]?
-                                .Where(_ => _["type"].Value<string>() == "ProductLane")
-                                .ToList();
-
-                            var legends = productLanes
-                                .SelectMany(_ => _["_embedded"]?["items"])
-                                .Where(_ => _["type"]?.Value<string>() == "Legend");
-                            if (legends.Any())
+                            foreach (var cards in data.RootElement.GetProperty("cards").EnumerateArray())
                             {
-                                productCategoryUrls.AddRange(legends.Select(_ => _["navItem"]?["link"]?["href"].Value<string>()));
+                                foreach (var product in cards.GetProperty("products").EnumerateArray())
+                                {
+                                    productUrls.Add(product.GetProperty("link").ToString());
+                                }
                             }
-
-                            var seeMoreItems = data["_embedded"]?["lanes"]?
-                                .Where(_ => _["type"].Value<string>() == "SeeMoreLane")
-                                .SelectMany(_ => _["_embedded"]?["items"]);
-                            if (seeMoreItems.Any())
-                            {
-                                productCategoryUrls.AddRange(seeMoreItems.Select(_ => _["navItem"]?["link"]?["href"].Value<string>()));
-                            }
-                        }
-                        else
-                        {
-                            throw new ArgumentException("No data loaded");
                         }
                     }
                 }
+                var logLineEnd = $"Found {productUrls.Count} products.";
+                Console.WriteLine(logLineEnd);
+                _streamWriter.WriteLine(logLineEnd);
             }
             catch (Exception ex)
             {
-                _streamWriter.WriteLine($"Error scraping category: {url}");
+                _streamWriter.WriteLine($"Error scraping: {url}");
                 _streamWriter.WriteLine(ex);
-            }
-
-            //If there are sub categories found open these sub categories
-            if (productCategoryUrls.Any())
-            {
-                foreach (string productCategoryUrl in productCategoryUrls)
-                {
-                    productUrls.AddRange(await GetProductUrls(productCategoryUrl));
-                }
-            }
-            //If there are no sub categories found get product url's
-            else
-            {
-                var products = productLanes
-                    .SelectMany(_ => _["_embedded"]?["items"])
-                    .Where(_ => _["type"]?.Value<string>() == "Product");
-                productUrls.AddRange(products.Select(_ => "https://www.ah.nl" + _["navItem"]?["link"]?["href"].Value<string>()));
-
-                var logLine = $"Scraped category: {url}. Found {productUrls.Count} products.";
-                Console.WriteLine(logLine);
-                _streamWriter.WriteLine(logLine);
             }
 
             return productUrls;
@@ -172,6 +154,7 @@ namespace ProductScraper.Scrapers
         {
             try
             {
+                url = BaseUrl + url;
                 var productDocument = await _browsingContext.OpenAsync(url);
 
                 var code = "";
@@ -187,7 +170,7 @@ namespace ProductScraper.Scrapers
                 }
 
                 //Scrape product page
-                var name = productDocument.QuerySelector("div.product-hero h1 span").TextContent;
+                var name = productDocument.QuerySelector("article h1 span").TextContent;
                 name = Regex.Replace(name, @"[\u00AD]", ""); //Remove soft hypens from name
                 var ammount = GetAmmount(productDocument);
                 var ingredients = GetIngredients(productDocument);
@@ -234,15 +217,15 @@ namespace ProductScraper.Scrapers
                 @"allergie informatie.*",
                 @"allergie-informatie.*",
                 @"kan.*bevatten.*",
-                @"may contain.*"
+                @"may contain.*",
+                @"waarvan*"
             };
 
             var ingredients = "";
             try
             {
                 ingredients = productDocument
-                    .QuerySelectorAll("div.product-info-ingredients h2")
-                    .First(_ => _.TextContent == "ingrediënten")
+                    .QuerySelector(".product-info-ingredients h2")
                     .NextSibling.TextContent.ToLower();
             }
             catch
@@ -269,8 +252,7 @@ namespace ProductScraper.Scrapers
             try
             {
                 allergyInfo = productDocument
-                    .QuerySelectorAll("div.product-info-ingredients h4")
-                    .First(_ => _.TextContent == "allergie-informatie")
+                    .QuerySelector(".product-info-ingredients h4")
                     .NextSibling.TextContent.ToLower();
             }
             catch
@@ -288,12 +270,11 @@ namespace ProductScraper.Scrapers
 
         private string GetAmmount(IDocument productDocument)
         {
-            var ammount = productDocument.QuerySelector("div.product-hero h1")
-                    .NextSibling
-                    .ChildNodes
-                    .OfType<IText>()
-                    .Select(m => m.Text)
-                    .FirstOrDefault();
+            var ammount = productDocument
+                .QuerySelector("[class^='product-card-header_unitInfo']")
+                .ChildNodes.OfType<IText>()
+                .Select(m => m.Text)
+                .FirstOrDefault();
 
             if (!Regex.Match(ammount, @"\d+\s*x\s*[\d.,\s]+[gmkl][lg]*", RegexOptions.IgnoreCase).Success)
             {
@@ -317,12 +298,11 @@ namespace ProductScraper.Scrapers
 
         private bool GetIsStoreAdvertisedVegan(IDocument productDocument)
         {
-            var isVegan = productDocument.QuerySelector("li.productcard-info__feature svg--vegan") != null
-                || productDocument.QuerySelectorAll("li.productcard-info__feature p").Any(_ => _.TextContent.ToLower().Contains("vegan"));
+            var isVegan = productDocument.QuerySelectorAll("li.productcard-info__feature p").Any(_ => _.TextContent.ToLower().Contains("vegan"));
 
             if (!isVegan)
             {
-                var summaryNodes = productDocument.QuerySelector("div.product-hero div.product-summary");
+                var summaryNodes = productDocument.QuerySelector("article .product-summary");
                 isVegan = summaryNodes != null && summaryNodes.TextContent.ToLower().Contains("vegan");
             }
 
